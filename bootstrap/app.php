d4d9555bed44565a7830c96453758d7993b0c4aa
Dušan Osmokrović <60246031+d855@users.noreply.github.com>
2025-04-16 10:16:41 +0200

    renders json for api exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,5 +26,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(function (Request $request): bool {
+            if ($request->is('api/*')) {
+                return true;
+            }
+            return $request->wantsJson();
+        });
     })->create();
